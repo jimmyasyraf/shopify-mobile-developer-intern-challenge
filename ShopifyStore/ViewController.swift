c1c1200c1dd512c125431ce33a87540cc262a4b0
Hazimi Asyraf <jimmyasyraf@gmail.com>
2018-01-04 17:47:36 -0500

    Initial Commit

---
+++ ShopifyStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShopifyStore
+//
+//  Created by Hazimi Asyraf on 1/4/18.
+//  Copyright © 2018 Hazimi Asyraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
